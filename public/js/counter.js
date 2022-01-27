53a41c96787103ee54caafb108bd5b76ca96afe9
Craig Pastro <pastro.craig@gmail.com>
2022-01-26 21:06:30 -0800

    Update frontend to use DynamoDB

--- public/js/counter.js
+++ public/js/counter.js
@@ -1,10 +1,7 @@
-var counterContainer = document.querySelector(".counter");
-var visitCount = localStorage.getItem("page_view");
+const url = 'https://nn88u8jm0d.execute-api.us-west-2.amazonaws.com/Prod/counter/'
 
-if (!visitCount) {
-    visitCount = 0
-}
-
-visitCount = Number(visitCount) + 1;
-localStorage.setItem("page_view", visitCount)
-counterContainer.innerHTML = visitCount;
+fetch(url, { method: 'POST' })
+    .then(response => response.json())
+    .then(json => {
+        document.querySelector(".counter").innerHTML = Number(json.value);
+    });
